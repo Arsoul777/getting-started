89c919861c7424e6435e3519dacdb54e4a784187
Arsoul <oulhaciarslan@gmail.com>
2023-07-26 23:44:53 +0200

    Implement /hello page

--- server.js
+++ server.js
@@ -1,1 +1,30 @@
-console.log("Hello World");
+const express = require('express');
+const app = express();
+
+/** 
+ * @param {Express.Request} req 
+ * @param {Express.Response} res
+ * */
+function getHello(req, res) {
+    const params = req.query
+    const parsed = JSON.parse('{"hello": "world"}')
+    console.log(parsed);
+    // HTML, CSS, Ficher mp3
+    res.send(`<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <title> hello world ! </title>
+        <h1 style="color:tomato;">Hello, ${params.ARO}</h1>
+    </body>
+    </html>`);
+}
+
+app.get("/hello", getHello)
+
+app.listen(8080)
